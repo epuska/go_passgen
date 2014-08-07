db2c105a31e54e652e1e1ac36efd0278a3783a00
epuska <eerik.puska@gmail.com>
2014-08-07 17:46:08 +0700

    asks for user input now

--- src/main.go
+++ src/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"log"
+	"os"
+	"bufio"
+	"fmt"
 	"encoding/base64"
 	"crypto/sha256"
 	"code.google.com/p/go.crypto/pbkdf2"
+	"github.com/howeyc/gopass"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func clear(b []byte) {
     for i := 0; i < len(b); i++ {
@@ -29,8 +38,21 @@ func generatePassword(masterPassword, id []byte) []byte {
 }
 
 func main() {
-	masterPassword := []byte("tesstpa!#)ssword")
-	id := []byte("github")
+	fmt.Printf("Master password: ")
+	masterPassword := gopass.GetPasswd()
 	
-	log.Print(string(id) + ": " + base64.StdEncoding.EncodeToString(generatePassword(masterPassword, id)))
+	bio := bufio.NewReader(os.Stdin)
+	
+	fmt.Printf("Generate password for ID: ")
+	id, err := bio.ReadBytes('\n')
+	check(err)
+	id = id[:len(id)-1] // remove '\n' from the end
+	
+	password := generatePassword(masterPassword, id)
+		
+	// need to figure out better output mechanism than this:
+	fmt.Printf("Password for " + string(id) + ": " + base64.StdEncoding.EncodeToString(password) + "\n")
+	
+	clear(masterPassword)
+	clear(password)
 }
